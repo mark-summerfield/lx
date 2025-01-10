4aba7ba08aaf90c8ac61bcd2ba83c85f539c8484
mark-summerfield <mark@qtrac.eu>
2025-01-10 11:47:40 +0000

    m

--- fs.lua
+++ fs.lua
@@ -12,22 +12,6 @@ function Fs.chmod(filename, mode) -- Unix-specific
     os.execute("chmod " .. mode .. ' "' .. filename .. '"')
 end
 
-function Fs.copy(source, dest)
-    local infile = io.open(source, "rb")
-    if infile then
-        local oufile = io.open(dest, "wb")
-        if oufile then
-            oufile:write(infile:read("all"))
-            oufile:close()
-        else
-            error("Fs.copy() failed to write " .. dest)
-        end
-        infile:close()
-    else
-        error("Fs.copy() failed to read " .. source)
-    end
-end
-
 -- for remove use os.remove(filename); see also pl.{path,file,dir}
 
 return Fs
--- lx.lua
+++ lx.lua
@@ -7,20 +7,10 @@ package.path = package.path .. ";" .. ROCKS_PATH
 local ROCKS_SO_PATH = HOME .. "opt/luarocks/lib/lua/5.4/?.so"
 package.cpath = package.cpath .. ";" .. ROCKS_SO_PATH
 
-local function dumptbl(tbl, name)
-    if name then io.write(name, ": ") end
-    local sep = "{"
-    for key, value in pairs(tbl) do
-        io.write(sep, key, "=", value)
-        sep = ", "
-    end
-    io.write("}\n")
-end
-
 local function timeit(func)
     local t = os.clock()
     func()
     return os.clock() - t
 end
 
-return { dumptbl = dumptbl, timeit = timeit }
+return { timeit = timeit }
