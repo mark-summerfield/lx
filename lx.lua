02804d1a453cf7c06e9e828f78f30196c147ab60
mark-summerfield <mark@qtrac.eu>
2025-01-10 18:07:34 +0000

    m

--- lx.lua
+++ lx.lua
@@ -6,11 +6,3 @@ local ROCKS_PATH = HOME .. "opt/luarocks/share/lua/5.4/?.lua"
 package.path = package.path .. ";" .. ROCKS_PATH
 local ROCKS_SO_PATH = HOME .. "opt/luarocks/lib/lua/5.4/?.so"
 package.cpath = package.cpath .. ";" .. ROCKS_SO_PATH
-
-local function timeit(func)
-    local t = os.clock()
-    func()
-    return os.clock() - t
-end
-
-return { timeit = timeit }
