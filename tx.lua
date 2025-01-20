75070031d22ec65581d5d3b8d54206f7cfb7cc18
mark-summerfield <mark@qtrac.eu>
2025-01-20 11:36:08 +0000

    m

--- tx.lua
+++ tx.lua
@@ -26,4 +26,11 @@ end
 
 function tx.clone(tbl) return clone(tbl) end
 
+function tx.merge(tbl1, tbl2)
+    for key, value in pairs(tbl2) do
+        tbl1[key] = value
+    end
+    return tbl1
+end
+
 return tx
